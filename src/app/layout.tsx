3014f277c787a08c05861374fb2b7f2b1e23777e
Doiverson <doiverson.canada@gmail.com>
2024-11-28 17:57:44 -0800

    feat(layout): add async script for react-scan in the head section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <head>
+        <script src='https://unpkg.com/react-scan/dist/auto.global.js' async/>
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
